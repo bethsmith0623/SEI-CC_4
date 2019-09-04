f9788838b556f337047b6024cc8a724341dcfc2a
jim-clark <email@jim-clark.com>
2019-09-04 11:57:44 -0700

    Add intro-to-django-models lesson

---
+++ work/w08/d1/03-04-intro-to-django-models/completed-code/catcollector/main_app/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Cat(models.Model):
+  name = models.CharField(max_length=100)
+  breed = models.CharField(max_length=100)
+  description = models.TextField(max_length=250)
+  age = models.IntegerField()
+
+  def __str__(self):
+    return self.name
